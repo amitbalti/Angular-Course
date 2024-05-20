418b186f2841e8accfea320ad9c63c3988ebd229
Amit Balteriski <amitbalti@gmail.com>
2024-05-20 12:26:31 +0300

    push

--- Section 1 - Getting Started/my-first-app/src/app/app.module.ts
+++ Section 1 - Getting Started/my-first-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+// import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
